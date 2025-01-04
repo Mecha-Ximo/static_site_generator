136130ae41712eff8c25ec8bf6b57809c1944374
Ximo <ximo.dev.94@gmail.com>
2025-01-04 19:27:51 +0100

    add split nodes

--- src/test_utils.py
+++ src/test_utils.py
@@ -3,7 +3,7 @@ import unittest
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
 from leafnode import LeafNode
-from utils import text_node_to_html_node
+from utils import text_node_to_html_node, split_nodes_delimiter
 
 class TestUtils(unittest.TestCase):
     def test_text_to_html_TEXT(self):
@@ -53,3 +53,75 @@ class TestUtils(unittest.TestCase):
         html_node = text_node_to_html_node(text)
 
         self.assertEqual(html_node.to_html(), expected) 
+
+    def test_split_node_single(self):
+        text = TextNode("this is some text", TextType.TEXT)
+
+        result = split_nodes_delimiter([text], "*", TextType.ITALIC)
+
+        self.assertEqual([text], result)
+    
+    def test_split_node_italic(self):
+        text = TextNode("this is *some* text", TextType.TEXT)
+
+        result = split_nodes_delimiter([text], "*", TextType.ITALIC)
+
+        self.assertEqual([TextNode("this is ", TextType.TEXT),
+                          TextNode("some", TextType.ITALIC),
+                          TextNode(" text", TextType.TEXT)],
+                          result)
+    
+    def test_split_node_bold(self):
+        text = TextNode("this is **some** text", TextType.TEXT)
+
+        result = split_nodes_delimiter([text], "**", TextType.BOLD)
+
+        self.assertEqual([TextNode("this is ", TextType.TEXT),
+                          TextNode("some", TextType.BOLD),
+                          TextNode(" text", TextType.TEXT)],
+                          result)
+    
+    def test_split_node_bold(self):
+        text = TextNode("this is `some` text", TextType.TEXT)
+
+        result = split_nodes_delimiter([text], "`", TextType.CODE)
+
+        self.assertEqual([TextNode("this is ", TextType.TEXT),
+                          TextNode("some", TextType.CODE),
+                          TextNode(" text", TextType.TEXT)],
+                          result)
+    
+    def test_split_multi_nodes(self):
+        text1 = TextNode("this is *some* *text*", TextType.TEXT)
+        text2 = TextNode("this is *more*", TextType.TEXT)
+
+        result = split_nodes_delimiter([text1, text2], "*", TextType.ITALIC)
+
+        self.assertEqual([TextNode("this is ", TextType.TEXT),
+                    TextNode("some", TextType.ITALIC),
+                    TextNode(" ", TextType.TEXT),
+                    TextNode("text", TextType.ITALIC),
+                    TextNode("this is ", TextType.TEXT),
+                    TextNode("more", TextType.ITALIC),
+                    ],
+                    result)
+        
+    def test_split_no_nodes(self):
+        self.assertEqual(split_nodes_delimiter([], "*", TextType.ITALIC), [])
+    
+    def test_split_raise_if_wrong_delimiter(self):
+        self.assertRaises(Exception, lambda _: split_nodes_delimiter([], "=", TextType.CODE))
+
+    def test_split_multi_apply(self):
+        text1 = TextNode("this is *some* **text**", TextType.TEXT)
+
+        new_nodes = split_nodes_delimiter([text1], "**", TextType.BOLD)
+        new_nodes_2 = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
+
+        self.assertEqual([
+            TextNode("this is ", TextType.TEXT),
+            TextNode("some", TextType.ITALIC),
+            TextNode(" ", TextType.TEXT),
+            TextNode("text", TextType.BOLD)],
+            new_nodes_2
+            )
--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,28 @@ def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
                 "alt":text_node.text,
                 "src":text_node.url
             })
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter, text_type):
+    delimiters = ["*", "**", "`"]
+    if not delimiter in delimiters:
+        raise Exception("Unsupported delimiter")
+
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        parts = node.text.split(delimiter)
+
+        for i in range(0, len(parts)):
+            if parts[i] == "":
+                continue
+            
+            if i % 2 == 0:
+                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(parts[i], text_type))
+    
+    return new_nodes
